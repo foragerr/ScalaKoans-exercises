b6e20498ae1a7133406fddaabd699b12c6b37fc4
foragerr <foragerr@users.noreply.github.com>
2015-04-10 15:12:49 -0400

    completed UAP

--- src/test/scala/org/functionalkoans/forscala/AboutConstructors.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutConstructors.scala
@@ -14,14 +14,19 @@ class AboutConstructors extends KoanSuite {
 
   koan("Primary constructor specified with a parameter requires that parameter to be passed in") {
     val aboutMe = new AboutConstructorWithAuxiliaryConstructor()
-    aboutMe.name should be (__)
+    aboutMe.name should be ("defaultname")
   }
 
-  class AboutClassWithNoClassParameter
+  class AboutClassWithNoClassParameter(var something:Int){
+	def this(){
+		this(2)
+	}
+  }
 
   koan("Class with no class parameters is called with no arguments") {
     // add parameter to make this fail
-    val aboutMe = new AboutClassWithNoClassParameter
+    val aboutMe = new AboutClassWithNoClassParameter()
+	aboutMe.something should be (2)
 
   }
 }
